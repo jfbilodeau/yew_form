9d23a33b815645bbf5d115ea543fa57f23ccc055
Jacob Young <jacob@jry.io>
2021-12-23 18:33:26 -0500

    Migrate Checkbox to 0.19 Component trait

--- yew_form/src/checkbox.rs
+++ yew_form/src/checkbox.rs
@@ -1,11 +1,11 @@
-use crate::{Model, Form};
-use yew::{Callback, ComponentLink, Component, Html, html, Properties};
+use crate::{Form, Model};
+use yew::{html, Callback, Component, Context, Html, Properties};
 
 pub enum CheckBoxMessage {
     OnToggle,
 }
 
-#[derive(Properties, Clone)]
+#[derive(Properties, PartialEq, Clone)]
 pub struct CheckBoxProperties<T: Model> {
     pub field_name: String,
     pub form: Form<T>,
@@ -14,7 +14,6 @@ pub struct CheckBoxProperties<T: Model> {
 }
 
 pub struct CheckBox<T: Model> {
-    link: ComponentLink<Self>,
     props: CheckBoxProperties<T>,
 }
 
@@ -28,7 +27,9 @@ impl<T: Model> CheckBox<T> {
     fn set_value(&mut self, value: bool) {
         let field_path = &self.props.field_name;
 
-        self.props.form.set_field_value(field_path, &value.to_string());
+        self.props
+            .form
+            .set_field_value(field_path, &value.to_string());
     }
 }
 
@@ -36,14 +37,13 @@ impl<T: Model + Clone> Component for CheckBox<T> {
     type Message = CheckBoxMessage;
     type Properties = CheckBoxProperties<T>;
 
-    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+    fn create(ctx: &yew::Context<Self>) -> Self {
         Self {
-            link,
-            props,
+            props: *ctx.props(),
         }
     }
 
-    fn update(&mut self, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
         match msg {
             CheckBoxMessage::OnToggle => {
                 let value = !self.value();
@@ -54,20 +54,19 @@ impl<T: Model + Clone> Component for CheckBox<T> {
         }
     }
 
-    fn change(&mut self, _props: Self::Properties) -> bool {
+    fn changed(&mut self, ctx: &yew::Context<Self>) -> bool {
         true
     }
 
-    fn view(&self) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <input
                 type="checkbox"
-                value=self.props.field_name.clone()
-                onclick=self.link.callback(|_e| CheckBoxMessage::OnToggle)
-                checked=self.value()
+                value={ self.props.field_name.clone() }
+                onclick={ ctx.link().callback(|_| CheckBoxMessage::OnToggle)}
+                checked={ self.value() }
                 class="form-check-input form-input"
              />
         }
     }
 }
-
